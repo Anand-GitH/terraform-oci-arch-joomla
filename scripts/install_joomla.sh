443ee9cf782e0091e5bb873c665348fffbecfe23
Anand <anand6@buffalo.edu>
2024-07-16 18:17:45 -0700

    Update install_joomla.sh Joomla 5-1-2 tar is corrupt so using 5-1-1
    Update install_joomla.sh Joomla 5-1-2 tar is corrupt so using 5-1-1

--- scripts/install_joomla.sh
+++ scripts/install_joomla.sh
@@ -18,9 +18,9 @@ else
   cd /var/www/html
 fi
 
-wget https://downloads.joomla.org/us/cms/joomla5/5-1-2/Joomla_5-1-2-Stable-Full_Package.tar.gz
-tar zxvf Joomla_5-1-2-Stable-Full_Package.tar.gz
-rm -rf Joomla_5-1-2-Stable-Full_Package.tar.gz
+wget https://downloads.joomla.org/cms/joomla5/5-1-1/Joomla_5-1-1-Stable-Full_Package.tar.gz
+tar zxvf Joomla_5-1-1-Stable-Full_Package.tar.gz
+rm -rf Joomla_5-1-1-Stable-Full_Package.tar.gz
 if [[ $use_shared_storage == "true" ]]; then
 	cp ${joomla_shared_working_dir}/htaccess.txt ${joomla_shared_working_dir}/.htaccess
   chown apache:apache -R ${joomla_shared_working_dir}
